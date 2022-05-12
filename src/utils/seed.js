56e2f2fd98d98b6de996f1d964d34320d64ff727
tsaqifrazin1 <my@emailaddress.com>
2022-05-12 16:16:22 +0800

    fix deploy.yaml

--- src/utils/seed.js
+++ src/utils/seed.js
@@ -10,14 +10,14 @@ async function createTestUser() {
             firstname: 'test',
             lastname: 'admin',
             email: 'admin@mail.com',
-            password: bcrypt.hash('test', 12),
+            password: '12345678',
             role: "admin"
         });
         const user = new User({
             firstname: 'test',
             lastname: 'user',
             email: 'user@mail.com',
-            password: bcrypt.hash('test', 12),
+            password: '12345678',
             role: "user"
         });
         await admin.save();
